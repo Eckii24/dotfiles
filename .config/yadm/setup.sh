8ee965704970903e02b32a460876062196b0ec3b
Matthias Eck <matthias.eck93@gmail.com>
2024-12-27 19:30:23 +0100

    feat(yadm): add classes based on ENV
    fix(yadm): looping through env variables
    
    fix(yadm): typo

--- .config/yadm/setup.sh
+++ .config/yadm/setup.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 echo "Starting setup..."
@@ -7,6 +8,16 @@ install_temp_yadm() {
     echo "Installing temporary YADM..."
     curl -fLo /tmp/yadm https://github.com/yadm-dev/yadm/raw/master/yadm
     chmod +x /tmp/yadm
+}
+
+# Set additional YADM classes if YADM_CLASSES environment variable is set
+set_yadm_classes() {
+    if [ -n "$YADM_CLASSES" ]; then
+        for class in "$YADM_CLASSES"; do
+            echo "Adding YADM class: $class"
+            /tmp/yadm config --add local.class "$class"
+        done
+    fi
 }
 
 # Clone dotfiles and invoke bootstrap
@@ -21,15 +32,18 @@ cleanup_temp_yadm() {
     rm -f /tmp/yadm
 }
 
-# Install temporary YADM, clone dotfiles, and clean up
+# Install temporary YADM, set classes, clone dotfiles, and clean up
 if ! command -v yadm &>/dev/null; then
     install_temp_yadm
+    set_yadm_classes
     clone_dotfiles
     cleanup_temp_yadm
 else
     echo "YADM is already installed. Cloning dotfiles..."
+    set_yadm_classes
     clone_dotfiles
 fi
 
 echo "Setup complete!"
+
 
