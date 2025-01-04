cc60bd7a528b7ff16b05295d92d452c5ed6f9625
Matthias Eck <matthias_eck@icloud.com>
2025-01-04 14:55:22 +0100

    feat(kanata): add kanata config

--- .config/zsh/100.aliases-macos.zsh##os.Darwin,e.zsh
+++ .config/zsh/100.aliases-macos.zsh##os.Darwin,e.zsh
@@ -1,2 +1,4 @@
 alias mitmweb='docker run -it --rm -v ~/.mitmproxy:/home/mitmproxy/.mitmproxy -p 8080:8080 -p 127.0.0.1:8081:8081 mitmproxy/mitmproxy mitmweb --web-host 0.0.0.0'
 alias icloud='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs'
+
+alias kanata='sudo kanata -c $HOME/.config/kanata/kanata.kdb'
