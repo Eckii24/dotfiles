b54406a4598a0a5080410e399bba5d09d357d686
Matthias Eck <matthias_eck@icloud.com>
2025-01-05 22:31:41 +0100

    feat(nvim): add keymaps for new lines and diff modes

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- Terminal Mappings
 vim.keymap.set("n", "<c-t>", function() Snacks.terminal(nil) end, { desc = "Terminal (Root Dir)" })
-vim.keymap.set("t", "<C-t>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+vim.keymap.set("t", "<C-t>", "<cmd>close<CR>", { desc = "Hide Terminal" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- AI completion
 vim.keymap.set("i", ",,m", require("copilot.suggestion").next, { desc = "Next AI suggestion" })
 vim.keymap.set("i", ",,n", require("copilot.suggestion").prev, { desc = "Previous AI suggestion" })
+
+-- New lines
+vim.keymap.set("n", "<leader>o", "o<Esc>0D", { desc = "which_key_ignore" })
+vim.keymap.set("n", "<leader>O", "O<Esc>0D", { desc = "which_key_ignore" })
+
+-- Diffs
+vim.keymap.set("n", "<leader>fd", "", { desc = "+diff"})
+vim.keymap.set("n", "<leader>fda", "<cmd>windo diffthis<CR>", { desc = "Diff this all windows"})
+vim.keymap.set("n", "<leader>fdo", "<cmd>diffoff!<CR>", { desc = "Diff off all windows"})
+vim.keymap.set("n", "<leader>fdd", function()
+  vim.cmd("diffoff!")
+  vim.cmd("only")
+end, { desc = "Diff off all windows and close inactive windows"})
