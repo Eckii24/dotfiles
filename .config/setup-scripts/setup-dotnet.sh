4f5d7ca6f489c7b2f560fc81213fdfe20925b8a3
Matthias Eck <matthias_eck@icloud.com>
2025-01-07 16:32:26 +0100

    feat(setup): add brew to path in setup dotnet

--- .config/setup-scripts/setup-dotnet.sh
+++ .config/setup-scripts/setup-dotnet.sh
@@ -2,6 +2,26 @@
 
 echo "Starting setup for dotnet..."
 
+# Function to add Homebrew to PATH
+if ! command -v brew &>/dev/null; then
+  echo "Homebrew not found. Adding to PATH..."
+  if [[  -x "/opt/homebrew/bin/brew"  ]]; then
+    # Apple Silicon (ARM)
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  elif [[  -x "/usr/local/bin/brew"  ]]; then
+    # Intel Mac
+    eval "$(/usr/local/bin/brew shellenv)"
+  elif [[  -x "/home/linuxbrew/.linuxbrew/bin/brew"  ]]; then
+    # Linux
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  else
+    echo "Homebrew not found. Exiting..."
+    exit 1
+  fi
+else
+  echo "Homebrew already added to PATH."
+fi
+
 echo "Set YADM class dotnet"
 yadm config --add local.class dotnet
 
@@ -10,9 +30,9 @@ echo "Install dependencies for dotnet..."
 if [[ "$(uname)" == "Linux" ]]; then
 
   if command -v sudo &>/dev/null && sudo -n true 2>/dev/null; then
-    sudo apt-get update && sudo apt-get install -y dotnet-sdk-9.0
+    sudo apt-get update && sudo apt-get install -y dotnet-sdk-8.0
   else
-    apt-get update && apt-get install -y dotnet-sdk-9.0
+    apt-get update && apt-get install -y dotnet-sdk-8.0
   fi
 else
   brew install --cask dotnet-sdk
