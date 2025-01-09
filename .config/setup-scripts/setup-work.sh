0e4f536e09d25791486d0ee8676eebcc7d084a46
Eckii24 <35373554+Eckii24@users.noreply.github.com>
2025-01-09 10:28:50 +0100

    refactor(work): set brew in setup if not set

--- .config/setup-scripts/setup-work.sh
+++ .config/setup-scripts/setup-work.sh
@@ -2,6 +2,26 @@
 
 echo "Starting setup for work environment..."
 
+# Function to add Homebrew to PATH
+if ! command -v brew &>/dev/null; then
+  echo "Homebrew not found. Adding to PATH..."
+  if [[  -x "/opt/homebrew/bin/brew"  ]]; then
+    # Apple Silicon (ARM)
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  elif [[  -x "/usr/local/bin/brew"  ]]; then
+    # Intel Mac
+    eval "$(/usr/local/bin/brew shellenv)"
+  elif [[  -x "/home/linuxbrew/.linuxbrew/bin/brew"  ]]; then
+    # Linux
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  else
+    echo "Homebrew not found. Exiting..."
+    exit 1
+  fi
+else
+  echo "Homebrew already added to PATH."
+fi
+
 echo "Set YADM class work"
 yadm config --add local.class work
 
