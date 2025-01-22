6a6f59157d643041c853730642d4d58a1e885227
Matthias Eck <matthias.eck@zeiss.com>
2025-01-22 07:37:58 +0000

    feat(zsh): add history

---
+++ .config/zsh/010.history.zsh
@@ -0,0 +1,6 @@
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=50000
+
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
