5fdebc5a3cbc67113453293082517f4ff993eb08
Matthias Eck <matthias.eck@zeiss.com>
2025-02-05 21:21:38 +0000

    feat(nvim): add sql and rest to work setup and setup dadbod

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,8 @@
 -- Add any additional options here
 vim.g.autoformat = false
 vim.opt.colorcolumn = "100"
+
+-- required secrets config, if the file exists
+if vim.fn.filereadable(vim.fn.expand("~/.config/nvim/lua/config/secrets.lua")) then
+  require("config.secrets")
+end
