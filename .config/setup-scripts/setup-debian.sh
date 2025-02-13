9c8e6dbeae8222d856edce50c0045083d7dd3026
Matthias Eck <matthias.eck@zeiss.com>
2025-02-13 12:39:47 +0100

    feat(debian): use official docker installation method

--- .config/setup-scripts/setup-debian.sh
+++ .config/setup-scripts/setup-debian.sh
@@ -38,9 +38,21 @@ sudo echo "net.core.wmem_max = 8388608" > /etc/sysctl.d/xrdp.conf
 
 # Install Docker
 echo "Installing Docker..."
-sudo apt install -y docker.io
-sudo systemctl enable docker
-sudo systemctl start docker # docker has to be started to be able to init dapr.
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Install dotfiles
 echo "Installing dotfiles..."
