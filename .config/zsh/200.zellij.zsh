3505f03d6b14f2b7f4d45f90b88e48d523c0fe17
Matthias Eck <matthias.eck@zeiss.com>
2025-02-17 12:58:02 +0100

    feat(zellij): adjust autostart, default layout and add alias for custom layout

--- .config/zsh/200.zellij.zsh
+++ .config/zsh/200.zellij.zsh
@@ -1,1 +1,6 @@
-eval "$(zellij setup --generate-auto-start zsh)"
+# Only start zellij if we are not in JetBrains IDEs
+if [[ "$TERMINAL_EMULATOR" != *"JetBrains-JediTerm"* ]]; then
+  eval "$(zellij setup --generate-auto-start zsh)"
+fi
+
+alias zellijc="zeelij --layout custom"
