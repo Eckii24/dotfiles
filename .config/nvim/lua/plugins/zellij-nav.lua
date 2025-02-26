465b5e54305d3e2f0512a9c79ea78226c70a068d
Matthias Eck <matthias.eck@zeiss.com>
2025-02-26 13:23:04 +0100

    feat(nvim): add more zellij related keymaps

--- .config/nvim/lua/plugins/zellij-nav.lua
+++ .config/nvim/lua/plugins/zellij-nav.lua
@@ -8,6 +8,10 @@ return {
       { "<c-j>", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down"  } },
       { "<c-k>", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up"    } },
       { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
+      { "<leader>wh", "<cmd>ZellijNavigateLeftTab<cr>",  { silent = true, desc = "navigate left or tab"  } },
+      { "<leader>wj", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down"  } },
+      { "<leader>wk", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up"    } },
+      { "<leader>wl", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
     },
     opts = {},
     init = function()
