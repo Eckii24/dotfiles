ea1db51896b7c6b36ea204ae2aa384a5c495ccc8
Matthias Eck <matthias_eck@icloud.com>
2025-03-01 22:48:08 +0100

    feat(zellij): enable auto attach and fix typo

--- .config/zsh/200.zellij.zsh
+++ .config/zsh/200.zellij.zsh
@@ -1,6 +1,7 @@
 # Only start zellij if we are not in JetBrains IDEs
 if [[ "$TERMINAL_EMULATOR" != *"JetBrains-JediTerm"* ]]; then
+  export ZELLIJ_AUTO_ATTACH="true"
   eval "$(zellij setup --generate-auto-start zsh)"
 fi
 
-alias zellijc="zeelij --layout custom"
+alias zellijc="zellij --layout custom"
