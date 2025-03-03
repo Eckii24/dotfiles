83fb22cb1ab0510b33daf6b2348b4522f6c8f0b6
Matthias Eck <matthias.eck@zeiss.com>
2025-03-03 08:08:21 +0100

    feat(aider): use aider config file

--- .config/zsh/200.aider.zsh
+++ .config/zsh/200.aider.zsh
@@ -1,16 +1,4 @@
 # Configuration of aider
-export AIDER_MODEL="azure/gpt"
+export AIDER_CONFIG="$HOME/.config/aider/aider.config.yml"
 
-export AIDER_MODEL_SETTINGS_FILE="$HOME/.config/aider/aider.model.settings.yml"
-export AIDER_MODEL_METADATA_FILE="$HOME/.config/aider/aider.model.metadata.json"
-
-export AIDER_COMPLETION_MENU_BG_COLOR="#ced6f7"
-export AIDER_COMPLETION_MENU_CURRENT_BG_COLOR="#ced6f7"
-export AIDER_COMPLETION_MENU_COLOR="#212139"
-export AIDER_COMPLETION_MENU_CURRENT_COLOR="#d65a00"
-
-export AIDER_DARK_MODE="true"
-export AIDER_VIM="true"
-export AIDER_EDITOR="nvim"
-
-export AIDER_READ=$HOME/.config/aider/Conventions.md
+alias aider="aider --config $AIDER_CONFIG"
