4fd11d0d61cdcfa566c3220c1d984dfe9c0ce77d
Matthias Eck <matthias_eck@icloud.com>
2025-03-06 21:01:57 +0100

    feat(zellij): deactivate auto attach and enable quit on force close

--- .config/zsh/200.zellij.zsh
+++ .config/zsh/200.zellij.zsh
@@ -1,10 +1,5 @@
 # Only start zellij if we are not in JetBrains IDEs
 if [[ "$TERMINAL_EMULATOR" != *"JetBrains-JediTerm"* ]]; then
-  # Auto attaching leads to issues on the linux VM.
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    export ZELLIJ_AUTO_ATTACH="true"
-  fi
-
   eval "$(zellij setup --generate-auto-start zsh)"
 fi
 
