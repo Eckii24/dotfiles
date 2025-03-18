a154f1720105c1c4c51bbbe29481fcebf165feb4
Matthias Eck <matthias_eck@icloud.com>
2025-03-19 00:28:46 +0100

    refactor(neovim): simplify mcp integration into codecompanion

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -4,22 +4,14 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
     },
-    cmd = "MCPHub", -- lazily start the hub when `MCPHub` is called
+    cmd = "MCPHub",
     build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module"ravitemer/mcphub.nvim",
     keys = {
       { "<leader>am", "<cmd>MCPHub<cr>", mode = "n", desc = "MCPHub" },
     },
     opts = {
-      -- Required options
-      port = 3000, -- Port for MCP Hub server
-      config = vim.fn.expand("~/.config/nvim/mcpservers.json"), -- Absolute path to config file
-      shutdown_delay = 0, -- Wait 0ms before shutting down server after last client exits
-      log = {
-        level = vim.log.levels.WARN,
-        to_file = false,
-        file_path = nil,
-        prefix = "MCPHub",
-      },
+      port = 3000,
+      config = vim.fn.expand("~/.config/nvim/mcpservers.json"),
     },
   },
   {
