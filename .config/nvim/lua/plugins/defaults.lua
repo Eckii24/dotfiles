5a74360dd5c189dc87712e53a857e986d9680fe2
Matthias Eck <matthias.eck@zeiss.com>
2025-04-22 08:23:21 +0200

    refactor(neovim): show terminal as floating window

--- .config/nvim/lua/plugins/defaults.lua
+++ .config/nvim/lua/plugins/defaults.lua
@@ -31,16 +31,41 @@ return {
         search = {
           enabled = true,
         },
-      }
-    }
+      },
+    },
   },
   {
     "folke/snacks.nvim",
     keys = {
-      { "<leader>gB", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
-      { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
-      { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
-    }
+      {
+        "<leader>gB",
+        function()
+          Snacks.picker.git_branches()
+        end,
+        desc = "Git Branches",
+      },
+      {
+        "<leader>gL",
+        function()
+          Snacks.picker.git_log_line()
+        end,
+        desc = "Git Log Line",
+      },
+      {
+        "<leader>gf",
+        function()
+          Snacks.picker.git_log_file()
+        end,
+        desc = "Git Log File",
+      },
+    },
+    opts = {
+      terminal = {
+        win = {
+          style = "float",
+        },
+      },
+    },
   },
   {
     "folke/todo-comments.nvim",
@@ -50,54 +75,56 @@ return {
       },
       search = {
         pattern = [[\b(KEYWORDS)]],
-      }
-    }
+      },
+    },
   },
   {
     "lewis6991/gitsigns.nvim",
     keys = {
       {
-        "<leader>h", "", desc = "+hunks",
+        "<leader>h",
+        "",
+        desc = "+hunks",
       },
       {
         "<leader>hb",
         "<cmd>Gitsigns blame_line<cr>",
-        desc = "Blame Line"
+        desc = "Blame Line",
       },
       {
         "<leader>hB",
         "<cmd>Gitsigns blame<cr>",
-        desc = "Blame Buffer"
+        desc = "Blame Buffer",
       },
       {
         "<leader>hs",
         "<cmd>Gitsigns stage_hunk<cr>",
-        desc = "Stage Hunk"
+        desc = "Stage Hunk",
       },
       {
         "<leader>hS",
         "<cmd>Gitsigns stage_buffer<cr>",
-        desc = "Stage Buffer"
+        desc = "Stage Buffer",
       },
       {
         "<leader>hr",
         "<cmd>Gitsigns reset_hunk<cr>",
-        desc = "Reset Hunk"
+        desc = "Reset Hunk",
       },
       {
         "<leader>hR",
         "<cmd>Gitsigns reset_buffer<cr>",
-        desc = "Reset Buffer"
+        desc = "Reset Buffer",
       },
       {
         "<leader>hu",
         "<cmd>Gitsigns undo_stage_hunk<cr>",
-        desc = "Undo Stage Hunk"
+        desc = "Undo Stage Hunk",
       },
       {
         "<leader>hp",
         "<cmd>Gitsigns preview_hunk_inline<cr>",
-        desc = "Preview Hunk Inline"
+        desc = "Preview Hunk Inline",
       },
     },
   },
