27fc96133cdcdc2f843d9810f4102d31efaa506a
Matthias Eck <matthias.eck@zeiss.com>
2025-05-20 11:07:48 +0200

    feat(Brewfile): add trash-cli

--- .config/zsh/010.aliases.zsh
+++ .config/zsh/010.aliases.zsh
@@ -5,7 +5,7 @@ alias lzd='lazydocker'
 alias ylg='lazygit --git-dir ~/.local/share/yadm/repo.git/'
 alias ynvim='yadm enter nvim ~'
 alias diff='delta -n -s'
-alias rm='rm -i'
+alias rm='trash-put'
 
 # Mac specific
 alias mitmweb='docker run -it --rm -v ~/.mitmproxy:/home/mitmproxy/.mitmproxy -p 8080:8080 -p 127.0.0.1:8081:8081 mitmproxy/mitmproxy mitmweb --web-host 0.0.0.0'
