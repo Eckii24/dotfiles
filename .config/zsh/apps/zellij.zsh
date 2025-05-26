574961f0f5597d4a9b76860ab05baa63d012b800
Matthias Eck <matthias.eck@zeiss.com>
2025-05-26 07:50:43 +0200

    refactor(zellij): autostart only for ssh sessions

--- .config/zsh/apps/zellij.zsh
+++ .config/zsh/apps/zellij.zsh
@@ -1,6 +1,7 @@
-# Only start zellij if we are not in JetBrains IDEs
-if [[ "$TERMINAL_EMULATOR" != *"JetBrains-JediTerm"* ]]; then
+# Only start zellij if we are in an SSH session
+if [[ -n "$SSH_CONNECTION" ]]; then
   eval "$(zellij setup --generate-auto-start zsh)"
 fi
 
 alias zellijc="zellij --layout custom"
+
