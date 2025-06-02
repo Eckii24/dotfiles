f130a2eea17a5d62e10e86d2e874d7079a32fc26
Matthias Eck <matthias.eck@zeiss.com>
2025-06-02 20:34:24 +0200

    feat(zsh): add alias for esc remapping

--- .config/zsh/010.aliases.zsh
+++ .config/zsh/010.aliases.zsh
@@ -7,6 +7,9 @@ alias ynvim='yadm enter nvim ~'
 alias diff='delta -n -s'
 alias rm='trash-put'
 
+# debian specific
+alias "map-esc"='setxkbmap -option caps:escape'
+
 # Mac specific
 alias mitmweb='docker run -it --rm -v ~/.mitmproxy:/home/mitmproxy/.mitmproxy -p 8080:8080 -p 127.0.0.1:8081:8081 mitmproxy/mitmproxy mitmweb --web-host 0.0.0.0'
 alias icloud='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs'
