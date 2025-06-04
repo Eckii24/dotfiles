53e515b6114e7d5108a528df2874ecc7e6ed73bd
Matthias Eck <matthias.eck@zeiss.com>
2025-06-04 22:48:20 +0200

    feat(zsh): add ignore parameter to assessment function

--- .config/zsh/functions/assessment.zsh
+++ .config/zsh/functions/assessment.zsh
@@ -3,6 +3,7 @@ function assessment() {
   local model="o4-mini"
   local output
   local branch
+  local ignore
 
   while [[ "$#" -gt 0 ]]; do
     case $1 in
@@ -22,6 +23,10 @@ function assessment() {
         branch="$2"
         shift 2
         ;;
+      --ignore | -i)
+        ignore="$2"
+        shift 2
+        ;;
       *)
         echo "Unknown parameter: $1"
         return 1
@@ -39,11 +44,13 @@ function assessment() {
     output=$(echo "$repo_url" | sed -E 's|https://github.com/||; s|/|-|g; s|$|.md|' | tr '[:upper:]' '[:lower:]')
   fi
 
-  if [[ -n "$branch" ]]; then
-    repomix --remote "$repo_url" --remote-branch "$branch"
-  else
-    repomix --remote "$repo_url"
-  fi
+  # Construct the repomix command dynamically
+  local repomix_cmd="repomix --remote \"$repo_url\""
+  [[ -n "$branch" ]] && repomix_cmd+=" --remote-branch \"$branch\""
+  [[ -n "$ignore" ]] && repomix_cmd+=" --ignore \"$ignore\""
+
+  # Execute the repomix command
+  eval "$repomix_cmd"
 
   # Pass the output to fabric -p check-assessment
   (cat repomix-output.md | fab -m "$model" -p check_assessment -o "$output")
