7ae0e07262c21c31f86d91e9290c389fbf6a87ab
Eckii24 <35373554+Eckii24@users.noreply.github.com>
2025-07-14 09:53:07 +0200

    refactor(setup): move docker to debian-ui

--- .config/setup-scripts/setup-debian-ui.sh
+++ .config/setup-scripts/setup-debian-ui.sh
@@ -6,6 +6,19 @@ set -e
 echo "Updating system..."
 sudo apt update && sudo apt upgrade -y
 
+# Install Docker
+echo "Installing Docker..."
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 # Run base Debian setup
 echo "Installing dotfiles..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Eckii24/dotfiles/refs/heads/master/.config/setup-scripts/setup-debian.sh)"
