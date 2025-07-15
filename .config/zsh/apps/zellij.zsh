cce875d90b4e051b5a3bf8c68f1bdaaa4554632f
Matthias Eck <matthias.eck@zeiss.com>
2025-07-15 14:19:00 +0200

    feat(zellij): adjust setting to auto attach

--- .config/zsh/apps/zellij.zsh
+++ .config/zsh/apps/zellij.zsh
@@ -1,6 +1,7 @@
 
 # Start zellij if in an SSH session or running inside WSL
 if [[ -n "$SSH_CONNECTION" || -n "$WSL_DISTRO_NAME" ]]; then
+  export ZELLIJ_AUTO_ATTACH=true
   eval "$(zellij setup --generate-auto-start zsh)"
 fi
 
