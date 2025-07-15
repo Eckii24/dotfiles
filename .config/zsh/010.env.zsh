dc497bfefd31412c4653650c769282518bd8ec6e
Matthias Eck <matthias.eck@zeiss.com>
2025-07-15 11:02:05 +0200

    fix(zsh): update PATH

--- .config/zsh/010.env.zsh
+++ .config/zsh/010.env.zsh
@@ -1,9 +1,8 @@
 export PROMPT_REGISTRY_PROJECT_DIR="$HOME/.config/promptregistry"
 export REPO_PATH="$HOME/Development/Repos"
 
+PATH="/opt/homebrew/opt/trash-cli/bin:$PATH"
 PATH="$PATH:$HOME/.dotnet/tools"
 PATH="$PATH:$HOME/.local/bin"
-PATH="$PATH:$HOME/.local/share/../bin"
 PATH="$PATH:$HOME/.local/share/bob/nvim-bin"
-PATH="/opt/homebrew/opt/trash-cli/bin:$PATH"
 export PATH
