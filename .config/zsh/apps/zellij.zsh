61ff58b06bae5e7e768d3305c9f53cbd4ab1c59b
Matthias Eck <matthias.eck@zeiss.com>
2025-07-16 22:32:44 +0200

    fix(zellij): dont use inside jetbrains terminal

--- .config/zsh/apps/zellij.zsh
+++ .config/zsh/apps/zellij.zsh
@@ -1,6 +1,7 @@
 
-# Start zellij if in an SSH session or running inside WSL
-if [[ -n "$SSH_CONNECTION" || -n "$WSL_DISTRO_NAME" ]]; then
+# Start zellij if in an SSH session or running inside WSL,
+# but skip if TERMINAL_EMULATOR is Jetbrains-JediTerm
+if [[ ( -n "$SSH_CONNECTION" || -n "$WSL_DISTRO_NAME" ) && "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" ]]; then
   export ZELLIJ_AUTO_ATTACH=true
   eval "$(zellij setup --generate-auto-start zsh)"
 fi
