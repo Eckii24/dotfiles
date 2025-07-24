e8996890c5c55de73741a24328b8ca3a49027388
Matthias Eck <matthias.eck@zeiss.com>
2025-07-24 22:16:04 +0200

    fix(zsh): add another ssl related env for WSL

--- .config/zsh/011.env.zsh##o.WSL,e.zsh
+++ .config/zsh/011.env.zsh##o.WSL,e.zsh
@@ -1,2 +1,3 @@
 export NODE_EXTRA_CA_CERTS=/usr/local/share/ca-certificates/zscaler-root-cert.crt
 export SSL_CERT_FILE=/usr/local/share/ca-certificates/zscaler-root-cert.crt
+export REQUESTS_CA_BUNDLE=/usr/local/share/ca-certificates/zscaler-root-cert.crt
