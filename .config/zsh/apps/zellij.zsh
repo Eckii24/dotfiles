2db0f7489f4f86fcbf73004a7cccf4bbf54f8531
Matthias Eck <matthias.eck@zeiss.com>
2025-08-05 20:56:06 +0200

    refactor(zellij): use tooltips and exclude inside vscode

--- .config/zsh/apps/zellij.zsh
+++ .config/zsh/apps/zellij.zsh
@@ -1,7 +1,7 @@
 
 # Start zellij if in an SSH session or running inside WSL,
 # but skip if TERMINAL_EMULATOR is Jetbrains-JediTerm
-if [[ ( -n "$SSH_CONNECTION" || -n "$WSL_DISTRO_NAME" ) && "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" ]]; then
+if [[ ( -n "$SSH_CONNECTION" || -n "$WSL_DISTRO_NAME" ) && "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" && "$TERM_PROGRAM" != "vscode" ]]; then
   export ZELLIJ_AUTO_ATTACH=true
   eval "$(zellij setup --generate-auto-start zsh)"
 fi
