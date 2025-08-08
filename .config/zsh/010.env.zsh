f5d407039769e1c2b674bee953f823a695029ed1
Matthias Eck <matthias.eck@zeiss.com>
2025-08-08 18:46:57 +0200

    feat(zsh): adding ZDG paths

--- .config/zsh/010.env.zsh
+++ .config/zsh/010.env.zsh
@@ -1,6 +1,12 @@
 export PROMPT_REGISTRY_PROJECT_DIR="$HOME/.config/promptregistry"
 export REPO_PATH="$HOME/Development/Repos"
 
+export XDG_BIN_DIR="$HOME/.local/bin"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_STATE_HOME="$HOME/.local/state"
+
 PATH="$PATH:$HOME/.dotnet/tools"
 PATH="$PATH:$HOME/.local/bin"
 PATH="$PATH:$HOME/.local/share/bob/nvim-bin"
