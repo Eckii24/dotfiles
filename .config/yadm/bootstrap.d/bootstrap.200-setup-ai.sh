757f93f482cdc4fdd40375bd1c3bb752d38b3f96
Matthias Eck <matthias.eck@zeiss.com>
2025-08-08 18:47:41 +0200

    feat(opencode): add opencode

--- .config/yadm/bootstrap.d/bootstrap.200-setup-ai.sh
+++ .config/yadm/bootstrap.d/bootstrap.200-setup-ai.sh
@@ -1,3 +1,6 @@
 echo "Install VectorCode..."
 uv tool install --force 'vectorcode[lsp,mcp]'
 echo "Run docker compose -f $HOME/.config/vectorcode/docker-compose.yaml up -d manually to start the chromadb for vectorcode."
+
+echo "Install OpenCode"
+curl -fsSL https://opencode.ai/install | bash
