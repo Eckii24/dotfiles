a07fad0a44572890b4654ed1b8b86a34d377e3ca
Matthias Eck <matthias_eck@icloud.com>
2025-08-18 18:29:15 +0200

    feat(nvim): customize dashboard

--- .config/nvim/lua/plugins/defaults.lua
+++ .config/nvim/lua/plugins/defaults.lua
@@ -93,6 +93,51 @@ return {
           },
         },
       },
+      dashboard = {
+        preset = {
+          keys = {
+            { icon = "󰚩", key = "a", desc = "CodeCompanion", action = ":CodeCompanionChat" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
+        sections = {
+          { section = "header" },
+          { section = "keys", gap = 1, padding = 2 },
+          {
+            icon = " ",
+            title = "Recent Files (cwd)",
+            section = "recent_files",
+            indent = 2,
+            padding = 2,
+            cwd = true,
+          },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2, cwd = false },
+          {
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "startup" },
+        },
+      },
     },
   },
   {
