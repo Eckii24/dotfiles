35271ef6b6135a459dec80f48a40e724c8489093
Matthias Eck <matthias_eck@icloud.com>
2025-09-10 22:37:31 +0200

    fix(nvim): disable roslyn lsp progress autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,17 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable Noice LSP progress for C# files, until the issue is resolved
+-- https://github.com/dotnet/roslyn/issues/79939
+-- https://github.com/folke/noice.nvim/issues/1144
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "cs" },
+  callback = function()
+    vim.api.nvim_clear_autocmds({
+      group = "noice_lsp_progress",
+      event = "LspProgress",
+      pattern = "*",
+    })
+  end,
+})
