6064aa8203c2cc0ec7704c6a4c7e7a8ad48d46fb
Matthias Eck <matthias.eck@zeiss.com>
2025-09-12 15:36:55 +0200

    refactor(nvim): use write mcp server to get work items

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -157,7 +157,7 @@ return {
                 role = "user",
                 content = [[
 ### Instructions
-Use the @{mcp} tool to load the details for an azure work item using MCP. 
+Use the @{ado__wit_get_work_item} tool to load the details for an azure work item using MCP. 
 Extract the description of the work item and present it in a useful way.
 
 ### Input
