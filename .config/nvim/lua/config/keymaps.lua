50945425d31c38d8055edd34fa075fa798af4af0
Matthias Eck <matthias.eck@zeiss.com>
2025-09-21 22:51:43 +0200

    Revert "refactor(nvim): use vim default clipboard, but remap yank and paste keymaps"
    This reverts commit 06c4ce952c989d0e019f709be9495a6a3493a5de.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,6 +9,9 @@ end, { desc = "Terminal (Root Dir)" })
 vim.keymap.set("t", "<C-t>", "<cmd>close<CR>", { desc = "Hide Terminal" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
+-- Copy / Paste
+vim.keymap.set("v", "p", '"_dP', { desc = "Paste without yanking" })
+
 -- LSP Restart
 vim.keymap.set("n", "<leader>cL", "<cmd>LspRestart<CR>", { desc = "Restart LSP" })
 
@@ -24,17 +27,6 @@ vim.keymap.set("n", "<leader>fdd", function()
   vim.cmd("diffoff!")
   vim.cmd("only")
 end, { desc = "Diff off all windows and close inactive windows" })
-
--- Yank always into system clipboard
-vim.keymap.set({ "n", "v" }, "y", '"+y', { desc = "Yank to clipboard" })
-vim.keymap.set("n", "Y", '"+Y', { desc = "Yank line to clipboard" })
-
--- Paste from system clipboard
-vim.keymap.set("n", "p", '"+p', { desc = "Paste from clipboard" })
-vim.keymap.set("n", "P", '"+P', { desc = "Paste before from clipboard" })
-
--- Visual paste: replace selection with clipboard contents, but don’t yank replaced text
-vim.keymap.set("v", "p", '"_d"+P', { desc = "Paste from clipboard without yanking" })
 
 -- Yanking file paths
 vim.keymap.set("n", "<leader>yp", function()
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,9 +4,6 @@
 -- Add any additional options here
 vim.opt.colorcolumn = "120"
 
--- Use the default vim clipboard behavior.
-vim.opt.clipboard = ""
-
 vim.lsp.config("roslyn", {
   settings = {
     ["csharp|inlay_hints"] = {
