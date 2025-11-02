1bdeee581200ab2fc016f9b1a92683aa255a6661
Matthias Eck <matthias_eck@icloud.com>
2025-11-02 11:24:59 +0100

    feat(yadm): add copilot and remove vectorcode for the moment

--- .config/yadm/bootstrap.d/bootstrap.200-setup-ai.sh
+++ .config/yadm/bootstrap.d/bootstrap.200-setup-ai.sh
@@ -1,10 +1,13 @@
-echo "Install VectorCode..."
-uv tool install --force 'vectorcode[lsp,mcp]'
-echo "Run docker compose -f $HOME/.config/vectorcode/docker-compose.yaml up -d manually to start the chromadb for vectorcode."
+# echo "Install VectorCode..."
+# uv tool install --force 'vectorcode[lsp,mcp]'
+# echo "Run docker compose -f $HOME/.config/vectorcode/docker-compose.yaml up -d manually to start the chromadb for vectorcode."
 
 echo "Install specify-cli..."
-uv tool install specify-cli --from git+https://github.com/github/spec-kit.git
+uv tool install specify-cli --force --from git+https://github.com/github/spec-kit.git
 
 echo "Install OpenCode"
 curl -fsSL https://opencode.ai/install | bash
 ln -sf "$HOME"/.opencode/bin/opencode "$HOME"/.local/bin/opencode
+
+echo "Install Github Copilot CLI..."
+npm install -g @github/copilot
