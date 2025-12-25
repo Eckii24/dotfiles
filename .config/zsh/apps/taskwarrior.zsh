00aab0050273ab9b47f73a5bb3571e958b5faf2f
Matthias Eck <matthias_eck@icloud.com>
2025-12-25 21:27:51 +0100

    feat(task): overwrite config paths for taskwarrior and taskopen

--- .config/zsh/apps/taskwarrior.zsh
+++ .config/zsh/apps/taskwarrior.zsh
@@ -1,4 +1,6 @@
 source $HOME/.config/zsh/apps/vimwiki.zsh
 
 export BUGWARRIORRC="$HOME/.config/task/bugwarrior.toml"
+export TASKRC="$HOME/.config/task/taskrc"
 export TASKDATA="$VIMWIKI_HOME/task"
+export TASKOPENRC="$HOME/.config/task/taskopenrc"
