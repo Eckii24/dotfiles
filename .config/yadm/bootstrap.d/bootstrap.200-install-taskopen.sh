80b7636246b3e8a42f65c901dc152eb86bc74d1a
Matthias Eck <matthias_eck@icloud.com>
2025-12-30 21:30:37 +0100

    feat(yadm): add taskopen install script

---
+++ .config/yadm/bootstrap.d/bootstrap.200-install-taskopen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -euo pipefail
+
+REPO=/tmp/taskopen
+
+# Remove any existing checkout to ensure a clean clone
+[ -d "$REPO" ] && rm -rf "$REPO"
+
+git clone https://github.com/jschlatow/taskopen.git "$REPO"
+cd "$REPO"
+
+make PREFIX="$HOME/.local"
+sudo make PREFIX="$HOME/.local" install
