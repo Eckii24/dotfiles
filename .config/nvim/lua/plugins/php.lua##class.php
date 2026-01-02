d977006500039c14453a6d281bb9406f42e50600
Matthias Eck <matthias_eck@icloud.com>
2026-01-02 11:55:01 +0100

    feat(nvim): add laravel support

---
+++ .config/nvim/lua/plugins/php.lua##class.php
@@ -0,0 +1,190 @@
+return {
+  {
+    "adalessa/laravel.nvim",
+    optional = true,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-neotest/nvim-nio",
+    },
+    cmd = { "Laravel" },
+    keys = {
+      {
+        "<leader>L",
+        desc = "Laravel",
+      },
+      {
+        "<leader>Ll",
+        function()
+          Laravel.pickers.laravel()
+        end,
+        desc = "Laravel: Open Laravel Picker",
+      },
+      {
+        "<c-g>",
+        function()
+          Laravel.commands.run("view:finder")
+        end,
+        desc = "Laravel: Open View Finder",
+      },
+      {
+        "<leader>La",
+        function()
+          Laravel.pickers.artisan()
+        end,
+        desc = "Laravel: Open Artisan Picker",
+      },
+      {
+        "<leader>Lt",
+        function()
+          Laravel.commands.run("actions")
+        end,
+        desc = "Laravel: Open Actions Picker",
+      },
+      {
+        "<leader>Lr",
+        function()
+          Laravel.pickers.routes()
+        end,
+        desc = "Laravel: Open Routes Picker",
+      },
+      {
+        "<leader>Lh",
+        function()
+          Laravel.run("artisan docs")
+        end,
+        desc = "Laravel: Open Documentation",
+      },
+      {
+        "<leader>Lm",
+        function()
+          Laravel.pickers.make()
+        end,
+        desc = "Laravel: Open Make Picker",
+      },
+      {
+        "<leader>Lc",
+        function()
+          Laravel.pickers.commands()
+        end,
+        desc = "Laravel: Open Commands Picker",
+      },
+      {
+        "<leader>Lo",
+        function()
+          Laravel.pickers.resources()
+        end,
+        desc = "Laravel: Open Resources Picker",
+      },
+      {
+        "<leader>Lp",
+        function()
+          Laravel.commands.run("command_center")
+        end,
+        desc = "Laravel: Open Command Center",
+      },
+      {
+        "gf",
+        function()
+          local ok, res = pcall(function()
+            if Laravel.app("gf").cursorOnResource() then
+              return "<cmd>lua Laravel.commands.run('gf')<cr>"
+            end
+          end)
+          if not ok or not res then
+            return "gf"
+          end
+          return res
+        end,
+        expr = true,
+        noremap = true,
+      },
+    },
+    opts = {
+      features = {
+        pickers = {
+          provider = "snacks",
+        },
+      },
+    },
+  },
+
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "blade-formatter",
+        "pint",
+        "rustywind",
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      local dap = require("dap")
+      dap.adapters.php = {
+        type = "executable",
+        command = "php-debug-adapter",
+        args = {},
+      }
+      dap.configurations.php = {
+        {
+          type = "php",
+          request = "launch",
+          name = "Listen for Xdebug",
+          port = 9003,
+          pathMappings = {
+            ["/var/www/html"] = "${workspaceFolder}",
+          },
+        },
+      }
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "phpdoc", "blade" } },
+  },
+
+  {
+    "ricardoramirezr/blade-nav.nvim",
+    optional = true,
+    ft = { "blade", "php" },
+  },
+
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "adalessa/laravel.nvim", "ricardoramirezr/blade-nav.nvim", "saghen/blink.compat" },
+    opts = {
+      sources = {
+        compat = { "blade-nav", "laravel" },
+        providers = {
+          ["blade-nav"] = {
+            name = "blade-nav",
+            module = "blade-nav.blink",
+          },
+          laravel = {
+            name = "laravel",
+            module = "blink.compat.source",
+            score_offset = 95,
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        blade = { "blade-formatter", "rustywind" },
+        php = { "php_cs_fixer", "pint" },
+      },
+    },
+  },
+}
