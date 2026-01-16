2d4024820cdeb807ee75b86b91b9d66d1e513eae
Matthias Eck <matthias.eck@zeiss.com>
2026-01-16 15:15:24 +0100

    refactor(zellij): adjust zellij settings

--- .config/nvim/lua/plugins/zellij-nav.lua
+++ .config/nvim/lua/plugins/zellij-nav.lua
@@ -4,21 +4,15 @@ return {
     lazy = true,
     event = "VeryLazy",
     keys = {
-      { "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>",  { silent = true, desc = "navigate left or tab"  } },
-      { "<c-j>", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down"  } },
-      { "<c-k>", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up"    } },
+      { "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>", { silent = true, desc = "navigate left or tab" } },
+      { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+      { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
       { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
-      { "<leader>wh", "<cmd>ZellijNavigateLeftTab<cr>",  { silent = true, desc = "navigate left or tab"  } },
-      { "<leader>wj", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down"  } },
-      { "<leader>wk", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up"    } },
+      { "<leader>wh", "<cmd>ZellijNavigateLeftTab<cr>", { silent = true, desc = "navigate left or tab" } },
+      { "<leader>wj", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+      { "<leader>wk", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
       { "<leader>wl", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
     },
     opts = {},
-    init = function()
-      vim.api.nvim_create_autocmd("VimLeave", {
-        pattern = "*",
-        command = "silent !zellij action switch-mode normal"
-      })
-    end
-  }
+  },
 }
