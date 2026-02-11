03ef747496a340ad3fd975c4385a8b86d7c26f0b
Matthias Eck <matthias.eck@zeiss.com>
2026-02-11 14:31:14 +0100

    feat(zsh): add function to replace ghe client id for opencode

--- .config/zsh/apps/opencode.zsh
+++ .config/zsh/apps/opencode.zsh
@@ -10,3 +10,16 @@ omo() {
     # Execute opencode with the updated config as environment variable
     OPENCODE_CONFIG_CONTENT="$updated_json" opencode "$@"
 }
+
+replace-opencode-ghe(){
+  local opencode_bin="$HOME/.opencode/bin/opencode"
+
+  # Backup the original binary
+  cp $opencode_bin "$opencode_bin.bak"
+
+  # Patch the ID
+  perl -pi -e 's/Ov23li8tweQw6odWQebz/Ov23ctDVkRmgkPke0Mmm/g' "$opencode_bin"
+
+  # Re-sign the binary (Crucial on macOS!)
+  sudo codesign --force --deep --sign - "$opencode_bin"
+}
