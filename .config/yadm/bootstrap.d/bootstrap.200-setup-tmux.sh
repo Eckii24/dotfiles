e95986c49c98fbdddbdbf414730f1e98bce0c823
Matthias Eck <matthias.eck@zeiss.com>
2026-03-02 09:30:24 +0100

    refactor(tmux): install tpm in config instead of script based

--- .config/yadm/bootstrap.d/bootstrap.200-setup-tmux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-echo "Install tmux plugin manager"
-mkdir -p "$HOME"/.config/tmux/plugins
-git clone https://github.com/tmux-plugins/tpm "$HOME"/.config/tmux/plugins/tpm
-tmux source "$HOME"/.config/tmux/tmux.conf
