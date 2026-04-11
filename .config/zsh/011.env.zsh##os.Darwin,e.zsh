e848d5ac9d710c38f3273f15163f858dea9bfdae
Matthias Eck <matthias_eck@icloud.com>
2026-04-11 10:38:21 +0200

    refactor(zsh): add latest php version

--- .config/zsh/011.env.zsh##os.Darwin,e.zsh
+++ .config/zsh/011.env.zsh##os.Darwin,e.zsh
@@ -2,6 +2,7 @@ PATH="/opt/homebrew/opt/trash-cli/bin:$PATH"
 
 # Herd injected PHP binary.
 PATH="$HOME/Library/Application Support/Herd/bin/":$PATH
+export HERD_PHP_85_INI_SCAN_DIR="$HOME/Library/Application Support/Herd/config/php/85/"
 export HERD_PHP_84_INI_SCAN_DIR="$HOME/Library/Application Support/Herd/config/php/84/"
 export HERD_PHP_83_INI_SCAN_DIR="$HOME/Library/Application Support/Herd/config/php/83/"
 export HERD_PHP_74_INI_SCAN_DIR="$HOME/Library/Application Support/Herd/config/php/74/"
