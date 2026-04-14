9651a3a099634ce0d3f4a7dbbd69829fdd969969
Matthias Eck <matthias.eck@zeiss.com>
2026-04-14 12:03:00 +0200

    fix(zsh): run-repo used wrong glob pattern

--- .config/zsh/functions/run-repo.zsh
+++ .config/zsh/functions/run-repo.zsh
@@ -1,12 +1,12 @@
 function run-repo(){
   if [[ $# -eq 0 ]]; then
     echo "Usage: $0 <command> [args...]"
-    exit 1
+    return 1
   fi
 
   COMMAND=("$@")
 
-  for sub_dir in "$REPO_PATH/*"; do
+  for sub_dir in $REPO_PATH/*; do
     if [[ -d $sub_dir ]]; then
       echo "Running '${COMMAND[@]}' in $sub_dir"
       (cd "$sub_dir" && "${COMMAND[@]}")
