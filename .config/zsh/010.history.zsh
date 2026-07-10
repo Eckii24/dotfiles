f85a7b19c904591543aef4b786f84119285838ca
Matthias Eck <matthias.eck@zeiss.com>
2026-07-10 13:02:26 +0200

    feat(zsh): disable history for leading space cmds

--- .config/zsh/010.history.zsh
+++ .config/zsh/010.history.zsh
@@ -4,3 +4,4 @@ SAVEHIST=50000
 
 setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_IGNORE_SPACE         # Ignore commands that start with a space.
