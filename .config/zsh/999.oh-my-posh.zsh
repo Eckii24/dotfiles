f02ddc32a562d3a33cb61eb992e45e2bac22bd70
Matthias Eck <matthias.eck@zeiss.com>
2026-07-14 13:31:50 +0200

    feat(herdr|oh-my-posh|zsh): add herdr support

--- .config/zsh/999.oh-my-posh.zsh
+++ .config/zsh/999.oh-my-posh.zsh
@@ -1,2 +1,8 @@
 # Must be last
-eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/theme.json)"
+
+if [[ -n "$HERDR_ENV" ]]; then
+  eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/herdr.json)"
+else
+  eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/theme.json)"
+fi
+
--- .config/zsh/apps/tmux.zsh
+++ .config/zsh/apps/tmux.zsh
@@ -1,4 +1,4 @@
-if [[ "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" && "$TERM_PROGRAM" != "vscode" && "$TERM" != tmux* && "$TERM" != screen* ]]; then
+if [[ "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" && "$TERM_PROGRAM" != "vscode" && "$TERM" != tmux* && "$TERM" != screen* && -z $HERDR_ENV ]]; then
   tmux new -A -s TMUX
 fi
 
